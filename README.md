79d0716a04a22f4031ec24e25023f77867976795
Glagny <nikoicardo@gmail.com>
2021-07-29 07:48:59 -0600

    Intro to FileSystem

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Node.js-First-Project
  An Introductory Project into Node.js
+
+The project is more intended to be a beginner's playground for myself. 
+
+Things used here: 
+- FileSystem: 'fs'
---
+++ app.js
@@ -0,0 +1,13 @@
+const fs = require('fs'); // create a filesystem object
+
+const userName = 'Niko'; 
+
+console.log(userName);
+
+fs.writeFile('user-data.txt', 'Name: ' + userName, (err) => {
+  if (err) {
+    console.log(err); 
+    return;
+  }
+  console.log('WROTE FILE');
+}); // write file is asynchronous 
